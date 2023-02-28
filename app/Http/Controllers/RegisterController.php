645bafe811fcf568781242f214b1802058ac77fa
Leskiv Andrii <leskivandrii21@gmail.com>
2023-02-28 17:53:44 +0200

    add controllers

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register() {
+        return '<h2>Register</h2><p>This is register page</p>';
+    }
+}
